13b8412d977731abb6d43b84776c5e1ec4fac387
MunChul Shin <mshin@redhat.com>
2023-09-13 17:58:46 -0700

    updating lombok models to correctly add the noArg constructor.

--- enterprise-application-code-generator/java/generative-archetype/model/lombok/domain-p0/model-lombok-domain-p0-archetype/src/main/resources/archetype-resources/src/main/java/__class0__.java
+++ enterprise-application-code-generator/java/generative-archetype/model/lombok/domain-p0/model-lombok-domain-p0-archetype/src/main/resources/archetype-resources/src/main/java/__class0__.java
@@ -3,12 +3,13 @@ package ${package};
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 /**
  * @author ${author}
  */
-@Data @AllArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor
 public class ${class0} implements Serializable {
 
     private static final long serialVersionUID = 1L;
