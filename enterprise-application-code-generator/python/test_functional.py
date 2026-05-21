37d455c5b72f127a3e170d50d8594cd9b32c9d09
MunChul Shin <mshin@redhat.com>
2026-05-20 21:15:20 -0700

    Incremental update. Added functional test descriptors for lombok models.

--- enterprise-application-code-generator/python/test_functional.py
+++ enterprise-application-code-generator/python/test_functional.py
@@ -23,26 +23,27 @@ if __name__ == "__main__":
     filef.clear_directory(ROOT_PATH / 'test_functional' / 'tmp')
 
     print('Running functional tests.')
+    print('This may take a few minutes...')
 
-    print('com.emprogen.java.maven.api.openapi.swagger.jaxrs.p5')
-    definition_path_str = str(CEJM_PATH / 'api' / 'openapi' / 'swagger' / 'jaxrs' / 'p5' / 'definition')
-    run_for_definition_path_str(definition_path_str)
+    # print('com.emprogen.java.maven.api.openapi.swagger.jaxrs.p5')
+    # definition_path_str = str(CEJM_PATH / 'api' / 'openapi' / 'swagger' / 'jaxrs' / 'p5' / 'definition')
+    # run_for_definition_path_str(definition_path_str)
 
-    print('com.emprogen.java.maven.service.jaxrs.quarkus.p2')
-    definition_path_str = str(CEJM_PATH / 'service' / 'jaxrs' / 'quarkus' / 'p2' / 'definition')
-    run_for_definition_path_str(definition_path_str)
+    # print('com.emprogen.java.maven.service.jaxrs.quarkus.p2')
+    # definition_path_str = str(CEJM_PATH / 'service' / 'jaxrs' / 'quarkus' / 'p2' / 'definition')
+    # run_for_definition_path_str(definition_path_str)
 
-    # print('com.emprogen.java.maven.model.lombok.domain.p1')
-    # definition_path = str(CEJM_PATH / 'model' / 'lombok' / 'domain' / 'p1' / 'definition')
-    # run_for_definition_path_str(definition_path)
+    print('com.emprogen.java.maven.model.lombok.domain.p1')
+    definition_path = str(CEJM_PATH / 'model' / 'lombok' / 'domain' / 'p1' / 'descriptor.yaml')
+    run_for_definition_path_str(definition_path)
 
-    # print('com.emprogen.java.maven.model.lombok.entity.p0')
-    # definition_path = str(CEJM_PATH / 'model' / 'lombok' / 'entity' / 'p0' / 'definition')
-    # run_for_definition_path_str(definition_path)
+    print('com.emprogen.java.maven.model.lombok.entity.p0')
+    definition_path = str(CEJM_PATH / 'model' / 'lombok' / 'entity' / 'p0' / 'definition')
+    run_for_definition_path_str(definition_path)
 
-    # print('com.emprogen.java.maven.model.lombok.jaxrs.p0')
-    # definition_path = str(CEJM_PATH / 'model' / 'lombok' / 'jaxrs' / 'p0' / 'definition')
-    # run_for_definition_path_str(definition_path)
+    print('com.emprogen.java.maven.model.lombok.jaxrs.p0')
+    definition_path = str(CEJM_PATH / 'model' / 'lombok' / 'jaxrs' / 'p0' / 'descriptor.yaml')
+    run_for_definition_path_str(definition_path)
 
     # print('com.emprogen.java.maven.service.jaxrs.springboot.camel.p0')
     # definition_path = str(CEJM_PATH / 'service' / 'jaxrs' / 'springboot' / 'camel' / 'p0' / 'definition')
